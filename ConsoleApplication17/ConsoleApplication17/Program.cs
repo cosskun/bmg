e07d8f4e649e720ac5202709684915c9b3d4347c
cosskun <34624521+cosskun@users.noreply.github.com>
2017-12-17 16:15:40 +0200

    Add files via upload

---
+++ ConsoleApplication17/ConsoleApplication17/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int a, b ,i; string ad; 
+            Console.Write("İsminizi Giriniz=");
+            ad = Console.ReadLine();
+            a = ad.Length;
+            for (i=0; i < a; i++)
+            {
+                b = (int)ad[i] + 10;
+                Console.Write((char)b);
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
